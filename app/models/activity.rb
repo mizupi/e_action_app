c73a989df15fe34e97ae238b4ed5872cfe6dc4d4
mizupi <mizukif2206@gmail.com>
2022-03-02 15:50:29 +0900

    actNameクラス作成

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,6 @@
 class Activity < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :act_name
+
+  validates :act_name_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_28_043329) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_02_022845) do
+  create_table "activities", charset: "utf8", force: :cascade do |t|
+    t.string "act_name_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false

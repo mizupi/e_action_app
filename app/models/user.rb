5821f62c2cfc1945796f13f30bdd5916009b397c
mizupi <mizukif2206@gmail.com>
2022-03-08 17:27:09 +0900

    relationshipモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,30 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :user_activities
+  has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
+  has_many :followings, through: :following_relationships
+  has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :follower_relationships
 
   validates :user_name, presence: true, length: { maximum: 10 }
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
+
+  # ともだち機能に関するメソッド
+  def following?(other_user)
+    following_relationships.find_by(following_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    following_relationships.create!(following_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    following_relationships.find_by(following_id: other_user.id).destroy
+  end
+  #友達判定
+  def matchers
+    followings & followers
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_03_021125) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_08_082001) do
   create_table "activities", charset: "utf8", force: :cascade do |t|
     t.string "activity", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", charset: "utf8", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "following_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follower_id", "following_id"], name: "index_relationships_on_follower_id_and_following_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+    t.index ["following_id"], name: "index_relationships_on_following_id"
   end
 
   create_table "user_activities", charset: "utf8", force: :cascade do |t|

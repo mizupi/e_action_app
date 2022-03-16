ed1aa796d813e72169676864437d5e096d32b464
mizupi <mizukif2206@gmail.com>
2022-03-16 15:03:22 +0900

    contactsコントローラーの作成

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,24 @@
+class ContactsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      ContactMailer.contact_mail(@contact, current_user).deliver
+      redirect_to root_path, notice: 'リクエストを送信しました'
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+    def contact_params
+      params.require(:contact).permit(:name, :content)
+    end
+end

57cdffc3ac8de1e7e4186baf227727ea0266d446
mizupi <mizukif2206@gmail.com>
2022-03-19 11:53:35 +0900

    アクション実績のアコーディオン

--- app/javascript/accordion.js
+++ app/javascript/accordion.js
@@ -1,33 +1,26 @@
 function accordions() {
+  // クリックする要素を取得
   const parents = 'accordion-open-heading-';
 	const dtObj = document.getElementsByTagName('dt');
+  // 'accordion-open-heading-'という文字列を含む正規表現オブジェクト生成
 	const dtMatch = new RegExp(parents);
-
-  for(var i = 0; i < dtObj.length; i++) {
-		if(dtObj[i].id.match(dtMatch)) {
-      dtObj[i].addEventListener("click", () => {
+  
+  // for文で各要素を分解してイベント付与
+  for(let i = 0; i < dtObj.length; i++) {
+    if(dtObj[i].id.match(dtMatch)) {
+      dtObj[i].addEventListener("click", function() {
+        // hiddenにしたい要素を取得
         const children = 'accordion-open-body-';
-        const ddObj = document.getElementById('dd');
+        const ddObj = document.getElementsByTagName('dd');
+        // 'accordion-open-body-'という文字列を含む正規表現オブジェクト生成
         const ddMatch = new RegExp(children)
-
-        for(i = 0; i < ddObj.length; i++) {
-          if(ddObj[i].id.match(ddMatch)) {
-            ddObj[i].classList.toggle("hidden");
-          };
+        // classにhiddenをaddして、toggleで切り替え
+        if(ddObj[i].id.match(ddMatch)) {
+          ddObj[i].classList.toggle("hidden");
         };
       });
 		};
 	};
-
-  // アコーディオンタイトルを取得
-//   const accordionTriggers = document.querySelectorAll(`#accordion-open-heading *`);
-//   console.log(accordionTriggers)
-//   accordionTriggers.addEventListener('click', () => {
-//     // 開閉させる要素を取得
-//     const content = document.querySelectorAll(`#accordion-open-body *`);
-//     // 'hidden'クラスを付与or削除
-//     content.classList.toggle("hidden");
-//   });
 };
 
 window.addEventListener('load', accordions)

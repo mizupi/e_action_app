967fb46067c092354ff8f6efb7a4c3f54ef86f86
mizupi <mizukif2206@gmail.com>
2022-03-19 12:32:52 +0900

    マイページのturbo無効にする

--- app/javascript/accordion.js
+++ app/javascript/accordion.js
@@ -2,7 +2,7 @@ function accordions() {
   // クリックする要素を取得
   const parents = 'accordion-open-heading-';
 	const dtObj = document.getElementsByTagName('dt');
-  // 'accordion-open-heading-'という文字列を含む正規表現オブジェクト生成
+  // 正規表現オブジェクト生成
 	const dtMatch = new RegExp(parents);
   
   // for文で各要素を分解してイベント付与
@@ -12,9 +12,9 @@ function accordions() {
         // hiddenにしたい要素を取得
         const children = 'accordion-open-body-';
         const ddObj = document.getElementsByTagName('dd');
-        // 'accordion-open-body-'という文字列を含む正規表現オブジェクト生成
+        // 正規表現オブジェクト生成
         const ddMatch = new RegExp(children)
-        // classにhiddenをaddして、toggleで切り替え
+        // クラスに付与して表示切り替え
         if(ddObj[i].id.match(ddMatch)) {
           ddObj[i].classList.toggle("hidden");
         };
